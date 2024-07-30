9e7fe564b8df74f5d6e7285af9f97919f24c23fb
MarcosBassetto <bassetto.marcos@gmail.com>
2024-07-29 23:46:45 -0300

    Primeiro commit

---
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -0,0 +1,35 @@
+package one.digitalinnovation.gof.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotNull(message = "Endereço é obrigatório")
+    @ManyToOne
+    private Endereco endereco;
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+    
+    
+}
